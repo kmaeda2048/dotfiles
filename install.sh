ef56db870d83dc14319fddaa02f4850030fc6de4
kmaeda2048 <48312376+kmaeda2048@users.noreply.github.com>
2020-09-06 18:36:56 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "$(curl https://raw.githubusercontent.com/kmaeda2048/dotfiles/master/.bashrc)" >> ~/.bashrc
+echo "$(curl https://raw.githubusercontent.com/kmaeda2048/dotfiles/master/.vimrc)" > ~/.vimrc
