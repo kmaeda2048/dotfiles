dfea4ab7a3ea2e54e93c4f1bc85eb2da1d903521
kmaeda2048 <48312376+kmaeda2048@users.noreply.github.com>
2020-09-12 22:37:19 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 echo "$(curl https://raw.githubusercontent.com/kmaeda2048/dotfiles/master/.bashrc)" >> ~/.bashrc
 echo "$(curl https://raw.githubusercontent.com/kmaeda2048/dotfiles/master/.vimrc)" > ~/.vimrc
+ln -s .vimrc .virc
 
